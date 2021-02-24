f20904dae894dc276606be409213a0262eeb9a46
smileysakai <senkuhatoki@hotmail.com>
2021-02-24 14:08:34 +1100

    Implemented Paypal functionality
    Payment via the checkout/view_cart page.
    So far payment records are kept in paypal sandbox and the cart does not lose it's items on payment success (maybe this could be expanded on in future).
    Sandbox details:
    https://www.sandbox.paypal.com
    
    Sandbox business account:
    sb-y1ewq5206132@business.example.com
    password: "s^6RbzW
    
    Sandbox Customer Account:
    sb-ic3im5206604@personal.example.com
    Password: Bn=W/K5F

--- drinks/views.py
+++ drinks/views.py
@@ -4,22 +4,32 @@ from .forms import DrinkForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from decimal import *
-from _overlapped import NULL
-
+from django.db.models import Sum
+#from _overlapped import NULL
 # Create your views here.
 def index(request):
+    #del request.session['cart']
     if 'cart' not in request.session:
             request.session['cart'] = {}
+    else:
+        cart_total_price=0
+        for key, value in request.session['cart'].items():
+            cart_total_price += Decimal(value['total_price'])
     return render(request, "drinks/index.html", {
         "drinks": Drink.objects.all(),
-        "cart":request.session['cart']
+        "cart":request.session['cart'],
+        "cart_total_price":str(cart_total_price)
     })
     
 def drink(request, drink_id):
     drink = Drink.objects.get(pk=drink_id)
+    cart_total_price=0
+    for key, value in request.session['cart'].items():
+        cart_total_price += Decimal(value['total_price'])
     return render(request, "drinks/drink.html", {
         "drink":drink,
-        "cart":request.session['cart']
+        "cart":request.session['cart'],
+        "cart_total_price":str(cart_total_price)
     })
     
 def add(request):
@@ -72,8 +82,17 @@ def add_to_cart(request, drink_id):
 def view_cart(request):    
     if 'cart' not in request.session:
             request.session['cart'] = {}
+    cart_total_price=0
+    cart_total_quantity=0
+    for key, value in request.session['cart'].items():
+        cart_total_price += Decimal(value['total_price'])
+        #cart_total_quantity += int(value['quantity'])
     return render(request, "drinks/view_cart.html", {
         "cart":request.session['cart'],
+        #"cart_total_quantity":str(cart_total_quantity),
+        "cart_total_price":str(cart_total_price),
+        #"test":str(cart_total_quantity) + "this string here" + str(cart_total_price)
+        #"test":Decimal(request.session['cart']['1']['total_price'])+Decimal(request.session['cart']['2']['total_price'])
         #"test":request.session['cart']['2']
         #found out that the 2nd argument in the comment above must be string
     })
@@ -92,3 +111,17 @@ def update_cartitem(request, drink_id):
         request.session['cart'][str(drink_id)]['total_price']=str(total_price)
         request.session.modified = True
     return HttpResponseRedirect(reverse("view_cart"))
+
+#def checkout(request):
+    # Creating Access Token for Sandbox
+   # client_id = "AZci21POXG3JTRRsL0Lt_FY2m43muEA36bKYKOvATmhRmiNfWqOtG_JV3RJymqtjrIKTL2-9nw_R844k"
+   # client_secret = "EDdNNU3blvqbb17wT7S_djKxXEsmz2f-B5qK70H_ZBb2rvPgiZE7FaXFmdhEmZejGutZApNcNaj2qxOk"
+    # Creating an environment
+   # environment = SandboxEnvironment(client_id=client_id, client_secret=client_secret)
+   # client = PayPalHttpClient(environment)
+
+
+
+
+
+
